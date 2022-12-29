9abc87662971acbfb1e5985b6c3bcf755032b8be
Shabana Noor <rineshabana95@gmail.com>
2022-12-29 20:50:09 +0530

    Fixed bug with login helper method by setting defaults

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 		content_tag(:div, "My content", class: 'my-class')	
 	end
 
-	def login_helper style
+	def login_helper style=''
 		if current_user.is_a? GuestUser 
 			
 			(link_to "Register",new_user_registration_path,class:style).html_safe +
